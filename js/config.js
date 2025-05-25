3783522f9e40332d652eeb92b7889cb5a46feee9
smp <71079258+bngjzx@users.noreply.github.com>
2025-05-25 14:35:09 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: '起源视频',
+    name: 'LibreTV',
     url: 'https://libretv.is-an.org',
     description: '免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
